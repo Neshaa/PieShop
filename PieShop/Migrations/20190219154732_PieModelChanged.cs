26156a38d56f5d13fd0e8b07611c217185d4d148
Nesha <nenad.zivanovic@intelisale.com>
2019-02-19 16:50:56 +0100

    finish module 5

---
+++ PieShop/Migrations/20190219154732_PieModelChanged.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PieShop.Migrations
+{
+    public partial class PieModelChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsInStock",
+                table: "Pies",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInStock",
+                table: "Pies");
+        }
+    }
+}
